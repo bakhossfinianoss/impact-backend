c96d671d7c141bf666e9b35aed835a1172a90835
bakhos <bakhoss.finianoss@gmail.com>
2024-10-09 01:24:01 +0300

    5000_port

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ mongoose.connect(dbConfig.mongoURI, {
 // Routes
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
